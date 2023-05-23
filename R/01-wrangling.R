686023e405747adb525fd91c517f74f99d2e0a27
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-05-22 19:40:44 -0700

    Wrangling script for Mendeley and Papers

---
+++ R/01-wrangling.R
@@ -0,0 +1,55 @@
+# ==============================================================================
+# Wrangling Reference Manager Data From Mendeley and Papers
+# ==============================================================================
+
+# Loading Packages 
+library(here)
+library(bib2df)
+library(feather)
+library(janitor)
+library(tidyverse)
+
+# Setting Constants
+RAW_DATA <- here("data", "raw")
+ACTIVE_DATA <- here("data", "active")
+
+# Loading Data Sets
+papers <- read_csv(here(RAW_DATA, "papers.csv")) %>%
+  clean_names() %>%
+  filter(!is.na(doi) | !is.na(pmid) | !is.na(pmcid),
+         !is.na(journal),
+         !is.na(abstract)) %>%
+  select(id = item_id_read_only, 
+         date_added = created_read_only, 
+         publication_year = year,
+         title,  author,  journal,  abstract, doi, pmid) %>%
+  mutate(date_added = ymd_hms(date_added),  reference_manager = "papers")
+
+mendeley <- bib2df(here(RAW_DATA, "/mendeley.bib")) %>%
+  clean_names() %>%
+  filter(!is.na(doi) | !is.na(pmid),
+         !is.na(journal),
+         !is.na(abstract)) %>%
+  select(publication_year = year, title, abstract, author, journal, doi, pmid) %>%
+  mutate(id = row_number(), .before = publication_year) %>%
+  mutate(author = str_flatten_comma(unlist(author)), .by = "id") %>%
+  mutate(publication_year = as.numeric(publication_year), 
+         date_added = date(NA),
+         reference_manager = "mendeley")
+
+# Bindings Both Data Sets
+articles <- rbind(papers, mendeley) %>%
+  # From duplicates, select the row from Papers
+  filter(!duplicated(doi, fromLast = TRUE)) %>%
+  filter(!duplicated(pmid), fromLast = TRUE)
+
+# Seperating into...
+# . Metadata: Descriptive information about an article
+# . Abstracts: The focus of the analysis
+# . Data sets are related by their `id` column
+metadata <- articles %>% select(-abstract)
+abstracts <- articles %>% select(id, abstract)
+
+# Saving
+write_feather(metadata, here(ACTIVE_DATA, "metadata.feather"))
+write_feather(abstracts, here(ACTIVE_DATA, "abstracts.feather"))
