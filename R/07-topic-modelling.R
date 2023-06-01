a90711f74cf720de416f99567dfe255c40bdab0f
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-05-31 21:49:47 -0700

    Adding publishing year and topic heatmap

--- R/07-topic-modelling.R
+++ R/07-topic-modelling.R
@@ -5,6 +5,7 @@
 # Loading Packages =============================================================
 library(here)
 library(broom)
+library(scales)
 library(feather)
 library(tidylda)
 library(tidytext)
@@ -21,11 +22,11 @@ theme_update(legend.position = "none",
 ACTIVE_DATA <- here("data", "active")
 DTM_PATH <- here(ACTIVE_DATA, 
                  "feature_engineering", 
-                 "tf_idf_matrix_long_3_0-05.feather")
+                 "tf_idf_matrix_long_3_2_0-05.feather")
 
 ETA <- 0.75
-ALPHA <- 0.25
-N_TOPICS <- 12
+ALPHA <- 0.01
+N_TOPICS <- 9
 ITERATIONS <- 500
 
 # Loading Data =================================================================
@@ -48,6 +49,7 @@ topic_document_probs <- topics %>%
   tidy(matrix = "theta")
 
 # Visualizations ===============================================================
+# . Top Words per Topic
 topic_labels <- setNames(str_c("Topic: ", 1:N_TOPICS), 1:N_TOPICS)
 topic_token_probs %>%
   slice_max(order_by = beta, n = 15, by = topic) %>%
@@ -56,14 +58,28 @@ topic_token_probs %>%
   ggplot(aes(x = token, y = beta, fill = topic)) +
     geom_col() +
     scale_x_reordered() +
+    labs(x = "Term", y = "Beta [P(Topic | Term)]") +
     coord_flip() +
     facet_wrap(~topic, scales = "free_y", labeller = as_labeller(topic_labels))
 
+# . Topic Timeline Heatmap
 topic_document_probs %>%
-  filter(document == "10") %>%
-  ggplot(aes(x = topic, y = theta)) +
-    geom_col()
-  
-read_feather(here("data", "active", "metadata.feather")) %>%
-  filter(id == "10") %>%
-  select(journal, title)
+  left_join(metadata %>% select(id, publication_year), 
+            by = c("document" = "id")) %>%
+  slice_max(order_by = theta, n = 1, by = document, with_ties = FALSE) %>%
+  count(year = factor(publication_year), topic = factor(topic), sort = TRUE) %>%
+  complete(year, topic, fill = list(n = 0)) %>%
+  mutate(prop = n / sum(n), .by = year) %>% {
+    ggplot(data = ., aes(x = topic, y = year, fill = prop)) +
+      geom_tile(color = "black") +
+      geom_tile(data = subset(., n == 0), fill = "black") +
+      geom_text(data = subset(., prop >= 0.2), 
+                aes(label = round(prop * 100)), 
+                size = 3,
+                color = "black") +
+      scale_fill_gradient2(low = "#381D2A", mid = "#AABD8C", high = "#E9E3B4",
+                           midpoint = 0.40, label = label_percent()) +
+      coord_equal() +
+      labs(x = "Topic", y = "Publication Year", fill = "P(Document | Topic)") +
+      theme(panel.grid = element_blank(), legend.position = "right")
+  }
