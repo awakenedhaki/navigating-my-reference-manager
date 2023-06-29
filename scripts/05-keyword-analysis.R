4550510dee4aa22be1993580f1c6086c8b06e098
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-06-29 00:19:36 -0700

    Re-defining keywords for authors and across documents

--- scripts/05-keyword-analysis.R
+++ scripts/05-keyword-analysis.R
@@ -8,7 +8,6 @@ library(udpipe)
 library(feather)
 library(tidytext)
 library(tidyverse)
-library(humaniformat)
 
 # Setting ggplot2 Defaults =====================================================
 update_geom_defaults("bar", list(color = "black", fill = "salmon"))
@@ -18,9 +17,16 @@ theme_update(panel.border = element_rect(color = "black", fill = NA))
 
 # Constants ====================================================================
 DATA <- here("data")
-MIN_TFIDF_WITHIN_DOC <- 0.2
 
-# Helper Functions =============================================================
+# Loading Data =================================================================
+metadata <- read_feather(here(DATA, "metadata.feather")) %>%
+  select(id, journal, publication_year, author, reference_manager)
+
+counts <- read_feather(here(DATA, "counts.feather")) %>%
+  select(id, term, n, tf_idf)
+
+# Keyword By Count =============================================================
+# . Helper Functions
 extract_authors <- function(tbl) {
   tbl %>%
     separate_longer_delim(cols = author, delim = ",") %>%
@@ -31,8 +37,13 @@ extract_keywords <- function(tbl, group = NULL, n_keywords = 10) {
   tbl %>%
     count({{ group }}, term, wt = n) %>%
     distinct(across({{ group }}), term, n) %>%
-    slice_max(order_by = n, n = n_keywords, by = {{ group }}) %>%
-    mutate(term = reorder_within(term, by = n, within = {{ group }},
+    slice_max(order_by = n, 
+              n = n_keywords, 
+              by = {{ group }},
+              with_ties = FALSE) %>%
+    mutate(term = reorder_within(str_to_upper(term), 
+                                 by = n, 
+                                 within = {{ group }},
                                  sep = ifelse(is.null({{ group }}), "", "___")))
 }
 
@@ -64,41 +75,34 @@ keyword_barplot <- function(tbl) {
     coord_flip()
 }
 
-# Loading Data =================================================================
-metadata <- read_feather(here(DATA, "metadata.feather")) %>%
-  select(id, journal, publication_year, author, reference_manager)
+# . Keyword Extraction
+term_by_authors <- counts %>%
+  left_join(metadata %>%
+              select(id, author) %>%
+              extract_authors() %>%
+              slice_tail(n = 3, by = id),
+            by = "id",
+            relationship = "many-to-many") %>%
+  select(-c(n, tf_idf))
 
-counts <- read_feather(here(DATA, "counts.feather")) %>%
-  select(id, term, n, tf_idf) %>%
-  left_join(metadata, by = "id")
+most_common_authors <- term_by_authors %>%
+  count(author) %>%
+  slice_max(order_by = n, n = 16, with_ties = FALSE) %>%
+  pull(author)
 
-# Keyword Analysis =============================================================
-# . Global Keywords
-counts %>%
-  filter(tf_idf > MIN_TFIDF_WITHIN_DOC) %>%
-  extract_keywords(n_keywords = 25) %>%
-  keyword_barplot()
-
-# . Journal Keywords
-counts %>%
-  filter(tf_idf > MIN_TFIDF_WITHIN_DOC) %>%
-  extract_keywords(journal, n_keywords = 10) %>%
-  select_from(journal, from = counts["journal"], n = 12) %>%
-  keyword_barplot() +
-    facet_wrap(~journal, scales = "free_y")
-
-# . Author Keywords
-counts %>%
-  extract_authors() %>%
-  select_from(author, from = extract_authors(metadata)) %>%
-  filter(tf_idf > MIN_TFIDF_WITHIN_DOC) %>%
-  extract_keywords(author) %>%
+term_by_authors %>%
+  count(author, term)  %>%
+  bind_tf_idf(term = term, document = author, n = n) %>%
+  filter(author %in% most_common_authors,
+         tf_idf > 0.01) %>%
+  extract_keywords(group = author, n_keywords = 8) %>%
   keyword_barplot() +
     facet_wrap(~author, scales = "free_y")
 
-# . Publication year 
-counts %>%
-  extract_keywords(publication_year) %>%
-  keyword_barplot() +
-    facet_wrap(~publication_year, scales = "free_y")
-
+# Collocation ==================================================================
+collocation <- counts %>%
+  filter(tf_idf > 0.25) %>%
+  mutate(term = str_to_upper(term)) %>%  filter(tf_idf > 0.25) %>%
+  keywords_collocation(term = "term",  group = "id", 
+                       ngram_max = 3) %>%
+  as_tibble()
