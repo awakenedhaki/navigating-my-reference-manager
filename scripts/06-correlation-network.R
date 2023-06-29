e4d6d3ff978ad5b9cbbcfc26081359e0f39bcd2d
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-06-28 21:53:58 -0700

    Using cosine similarity instead of pearson correlation

--- scripts/06-correlation-network.R
+++ scripts/06-correlation-network.R
@@ -29,7 +29,7 @@ MIN_VERTICES <- 5
 plot_graph <- function(graph, layout) {
   graph %>%
     ggraph(layout = layout) +
-      geom_edge_link(aes(alpha = correlation)) +
+      geom_edge_link(aes(alpha = similarity)) +
       geom_node_point(color = "black", size = 4) +
       geom_node_point() +
       geom_node_text(aes(label = name), repel = TRUE, family = "Roboto") +
@@ -43,23 +43,19 @@ counts <- read_feather(here(DATA, "counts.feather")) %>%
 
 # Term Correlation =============================================================
 # . Correlation (Adjacency) Matrix
-correlation_matrix <-  counts %>%
+similarity_matrix <-  counts %>%
   filter(total_n >= MIN_COUNT_ACROSS_CORPUS) %>%
-  pairwise_cor(term, id, sort = TRUE)
+  pairwise_similarity(item = term, feature = id, value = tf)
 
-# . Correlation Graph
-correlation_graph <- correlation_matrix %>%
-  filter(correlation > 0) %>%
-  slice_max(order_by = correlation, n = 1500) %>%
-  graph_from_data_frame()
-
-correlation_subgraphs <- decompose(correlation_graph, min.vertices = MIN_VERTICES) %>%
+similarity_graph <- similarity_matrix %>%
+  slice_max(order_by = similarity, n = 1500) %>%
+  graph_from_data_frame() %>%
+  decompose(min.vertices = MIN_VERTICES) %>%
   lapply(as_long_data_frame) %>%
   bind_rows() %>%
-  select(from_name, to_name, correlation) %>%
-  graph_from_data_frame()
+  select(from_name, to_name, similarity) %>%
+  graph_from_data_frame() 
 
 # Visualizations ===============================================================
-# . Components with more than two nodes
-correlation_subgraphs %>%
+similarity_graph %>%
   plot_graph(layout = "fr")
