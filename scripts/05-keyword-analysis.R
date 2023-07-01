078972375dd42977e95abe64dd28bf0d4dc16567
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-07-01 15:44:48 -0700

    Keywords of prevalent authors via TF-IDF

--- scripts/05-keyword-analysis.R
+++ scripts/05-keyword-analysis.R
@@ -17,42 +17,58 @@ theme_update(panel.border = element_rect(color = "black", fill = NA))
 
 # Constants ====================================================================
 DATA <- here("data")
+MIN_AUTHOR_APPEARANCE <- 3
 
 # Loading Data =================================================================
 metadata <- read_feather(here(DATA, "metadata.feather")) %>%
-  select(id, journal, publication_year, author, reference_manager)
+  select(id, journal, publication_year, reference_manager)
 
-counts <- read_feather(here(DATA, "counts.feather")) %>%
+authors <- read_feather(here(DATA, "cleaned", "author_names.feather")) %>%
+  add_count(author)
+
+counts <- read_feather(here(DATA, "features", "counts.feather")) %>%
   select(id, term, n, tf_idf)
 
-# Keyword Extraction ===========================================================
-term_by_authors <- counts %>%
-  left_join(metadata %>%
-              select(id, author) %>%
-              extract_authors() %>%
-              slice_tail(n = 3, by = id),
-            by = "id",
-            relationship = "many-to-many") %>%
-  select(-c(n, tf_idf))
+# Helper Functions =============================================================
+prevalence_weights <- function(position) {
+  0.5 * exp(abs(position))
+}
 
-most_common_authors <- term_by_authors %>%
-  count(author) %>%
-  slice_max(order_by = n, n = 16, with_ties = FALSE) %>%
+min_max_scaler <- function(x) {
+  minimum <- min(x)
+  maximum <- max(x)
+  
+  (x - minimum) / (maximum - minimum)
+}
+
+# Keyword Extraction ===========================================================
+# . By Author ==================================================================
+prevalent_authors <- authors %>%
+  group_by(id) %>%
+  mutate(author_order = row_number(),
+         median_centered_order = author_order - median(author_order),
+         weight = prevalence_weights(median_centered_order),
+         scaled_weight = min_max_scaler(weight)) %>%
+  ungroup() %>%
+  summarize(prevalence = sum(scaled_weight), .by = author) %>%
+  slice_max(order_by = prevalence, n = 16, with_ties = FALSE) %>%
   pull(author)
 
-term_by_authors %>%
-  count(author, term)  %>%
-  bind_tf_idf(term = term, document = author, n = n) %>%
-  filter(author %in% most_common_authors,
-         tf_idf > 0.01) %>%
-  extract_keywords(group = author, n_keywords = 8) %>%
-  keyword_barplot() +
-    facet_wrap(~author, scales = "free_y")
+author_tf_idf <- counts %>%
+  select(id, term) %>%
+  inner_join(authors[authors$n > MIN_AUTHOR_APPEARANCE, ], 
+             by = "id", relationship = "many-to-many") %>%
+  select(-c(id, n)) %>%
+  count(author, term) %>%
+  bind_tf_idf(term = term, document = author, n = n)
 
-# Collocation ==================================================================
-collocation <- counts %>%
-  filter(tf_idf > 0.25) %>%
-  mutate(term = str_to_upper(term)) %>%  filter(tf_idf > 0.25) %>%
-  keywords_collocation(term = "term",  group = "id", 
-                       ngram_max = 3) %>%
-  as_tibble()
+author_tf_idf %>%
+  filter(author %in% prevalent_authors) %>%
+  slice_max(order_by = tf_idf, n = 10, by = author, with_ties = FALSE) %>%
+  mutate(term = reorder_within(str_to_upper(term), 
+                               by = tf_idf, 
+                               within = author)) %>%
+  ggplot(aes(x = tf_idf, y = term)) +
+    geom_col() +
+    facet_wrap(~author, scales = "free_y", ncol = 4) +
+    scale_y_reordered()
